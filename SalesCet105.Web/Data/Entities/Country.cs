202859983341076fa94a3e651ca49a9b24eacb91
Daniel Kololo <Daniel.Kololo.18116@formandos.cinel.pt>
2026-07-30 03:15:04 +0100

    Init web project

---
+++ SalesCet105.Web/Data/Entities/Country.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SalesCet105.Web.Data.Entities
+{
+    public class Country
+    {
+
+        public int Id { get; set; }
+
+
+        [Display(Name = "País")]
+        [MaxLength(50, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres!")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório!")]
+        public string? Name { get; set; }
+    }
+}
